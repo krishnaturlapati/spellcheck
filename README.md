16d5f83f24878b48de5e36bfcea00946fd1ba80e
Venkata Turlapati <krishna@Venkatas-MBP.home>
2018-05-15 22:59:37 -0400

    rough cut 2

--- README.md
+++ README.md
@@ -7,3 +7,16 @@ Objectives:
 
 
 English dictionary source: https://raw.githubusercontent.com/matthewreagan/WebstersEnglishDictionary/master/dictionary.json
+
+Pickle words in dict 
+
+```
+>>> import pickle 
+>>> import json
+>>> data = open('dictionary.json', 'r').read()
+>>> eng_words=json.loads(data).keys()
+>>> eng_words_list=list(eng_words)
+>>> with open('dictwords.pkl', 'wb') as the_file:
+...     pickle.dump(eng_words_list, the_file)
+
+```
--- spellcheck.py
+++ spellcheck.py
@@ -2,22 +2,27 @@ import sys
 import os
 import json
 import difflib 
+import pickle
 
 
+pkl_file_path="dictwords.pkl"
 
-def load_json_file(path):
-    data = open(path, 'r').read()
+def load_pickle_file(pkl_file_path):
     try:
-        return list(json.loads(data).keys())
+	with open(pkl_file_path, 'rb') as the_file:
+		dict_words_list=pickle.load(the_file)
+	return dict_words_list
     except Exception as e:
 	print str(e)
 	print "Exiting spellcheck"
 	sys.exit()
 
-def spellcheck(sorted_ip_str, eng_dict_keys):
+
+
+def spellcheck(sorted_ip_str, dict_words_list):
 	misspelledwords=0
 	for word in sorted_ip_str:
-		if word not in eng_dict_keys:
+		if word not in dict_words_list:
 			print word
 			misspelledwords+=1
 	if misspelledwords==0:
@@ -26,9 +31,7 @@ def spellcheck(sorted_ip_str, eng_dict_keys):
 
 def main():
 	input_line=sys.argv[1]
-	eng_dict=sys.argv[2]
-	dict_words_list=load_json_file(eng_dict)	
-	
+	dict_words_list=load_pickle_file(pkl_file_path)	
 	ip_words=input_line.strip().split(' ')
  	uniq_ip_words=set(ip_words)
 	sorted_uniq_ip_words=sorted(list(uniq_ip_words))
