04e42d4f316280f0961cd401b2cf434691f298d3
Venkata Turlapati <krishna@Venkatas-MBP.home>
2018-05-16 20:52:28 -0400

    rough cut complete

--- spellcheck.py
+++ spellcheck.py
@@ -3,7 +3,7 @@ import os
 import json
 import difflib 
 import pickle
-
+import spell
 
 pkl_file_path="dictwords.pkl"
 
@@ -23,19 +23,25 @@ def spellcheck(sorted_ip_str, dict_words_list):
 	misspelledwords=0
 	for word in sorted_ip_str:
 		if word not in dict_words_list:
-			print word
+			return word
 			misspelledwords+=1
 	if misspelledwords==0:
 		print "found 0 misspelled words"
+		return None
 	
 
 def main():
+	miss_spelled_words=[]
 	input_line=sys.argv[1]
 	dict_words_list=load_pickle_file(pkl_file_path)	
 	ip_words=input_line.strip().split(' ')
  	uniq_ip_words=set(ip_words)
 	sorted_uniq_ip_words=sorted(list(uniq_ip_words))
-	spellcheck(sorted_uniq_ip_words, dict_words_list)	
+	miss_spelled_words.append(spellcheck(sorted_uniq_ip_words, dict_words_list))	
+	for word in miss_spelled_words:
+		print "misspelled word found: " + word + " " + ", possible correct word: " + str(spell.correction(word))
+		#print word
+
 
 if __name__=="__main__":
 	main()
